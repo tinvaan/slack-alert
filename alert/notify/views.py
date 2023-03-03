1d6d59c834d3fe21554bc0eb836853fdedae2586
Harish <harish.navnit@dialpad.com>
2023-03-03 09:43:54 +0530

    Update endpoint

--- alert/notify/views.py
+++ alert/notify/views.py
@@ -1,5 +1,5 @@
 
-from flask import Flask, request
+from flask import Flask, request, jsonify, make_response
 
 from .tasks import slack
 
@@ -9,12 +9,19 @@ app.config.from_object('alert.config')
 app.config.from_pyfile('config.py', silent=True)
 
 
-@app.route('/alert/notify/slack', methods=['POST'])
+@app.route('/alert/bounced', methods=['POST'])
 def notify():
     payload = request.get_json(force=True)
-    if (payload.get('TypeCode') == 512 and
-        payload.get('Type') == 'SpamNotification'
-    ):
-        return slack.send(payload)
+    if payload.get('RecordType') == 'Bounce':
+        if payload.get('TypeCode') == 512 and payload.get('Type') == 'SpamNotification':
+            return make_response(slack.send(payload), 202)
+
+        return jsonify({
+            'Type': payload.get('Type'),
+            'Email': payload.get('Email'),
+            'BouncedAt': payload.get('BouncedAt'),
+            'Description': payload.get('Description'),
+            'Tag': payload.get('Tag')
+        })
 
     return {'error': 'Bad Request'}, 400

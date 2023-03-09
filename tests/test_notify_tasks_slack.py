0ebd2139811a06cac835fd84a6982a8e64f511fc
Harish <harish.navnit@dialpad.com>
2023-03-09 08:36:52 +0530

    Fix tests

--- tests/test_notify_tasks_slack.py
+++ tests/test_notify_tasks_slack.py
@@ -1,13 +1,15 @@
 
+import json
 import pytest
 import unittest
 
-from alert.notify import app
+from alert.notify import app, queue
 from alert.notify.tasks import slack
 
 
 @pytest.mark.usefixtures('spam')
-class TestNotifyTasks(unittest.TestCase):
+@pytest.mark.usefixtures('bounce')
+class TestNotifySlack(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         app.config['TESTING'] = True
@@ -15,18 +17,27 @@ class TestNotifyTasks(unittest.TestCase):
         cls.config = app.config
 
     def setUp(self):
+        queue.conf.update(CELERY_ALWAYS_EAGER=True)
         self.url = 'http://%s:%s' % (self.config.get('ALERT_SERVICE_HOST'),
                                      self.config.get('ALERT_SERVICE_PORT'))
 
-    def test_bot(self):
+    def test_initialize(self):
         with self.app.app_context():
-            self.assertIsNotNone(slack.bot)
-            self.assertEqual(slack.bot.token, self.config.get('SLACK_CHANNEL_BOT_TOKEN'))
+            client = slack.initialize()
+            self.assertIsNotNone(client)
+            self.assertEqual(client.token, self.config.get('SLACK_CHANNEL_BOT_TOKEN'))
+
+    def test_content(self):
+        content = slack.content(self.spam)
+        self.assertIsInstance(json.loads(content), list)
+
+        content = slack.content(self.bounce)
+        self.assertIsInstance(json.loads(content), list)
 
     @pytest.mark.vcr
-    def test_send(self):
+    def test_notify(self):
         with self.app.app_context():
-            r = slack.send(payload=self.spam)
+            r = slack.notify(self.spam)
             self.assertIsNotNone(r)
             self.assertTrue(r.get('ok'))
             self.assertIsInstance(r.get('channel'), str)

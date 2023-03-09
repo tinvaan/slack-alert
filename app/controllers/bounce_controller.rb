4ad76426e9278e5b9682a5295d269822337e8fb0
Harish <harish.navnit@dialpad.com>
2023-03-09 20:53:10 +0530

    Fix API response

--- app/controllers/bounce_controller.rb
+++ app/controllers/bounce_controller.rb
@@ -6,16 +6,11 @@ class BounceController < ApplicationController
   def notify
     if @record && @record['RecordType'] == 'Bounce'
       if @record['Type'] == 'SpamNotification' && @record['TypeCode'] == 512
-        return SlackNotifyJob.set(queue: :spam).perform_now(@record) # TODO: perform_later
+        SlackNotifyJob.set(queue: :spam).perform_now(@record) # TODO: perform_later
+        return render status: 202, json: @response
       end
 
-      return render status: 200, json: {
-        'Type': @record['Type'],
-        'Email': @record['Email'],
-        'BouncedAt': @record['BouncedAt'],
-        'Description': @record['Description'],
-        'Tag': @record['Tag']
-      }
+      return render status: 200, json: @response
     end
 
     return render status: 400, json: { 'error': 'Bad Request' }
@@ -25,6 +20,13 @@ class BounceController < ApplicationController
 
   def parse
     @record = JSON.parse(request.body.read)
+    @response = {
+      'Type': @record['Type'],
+      'Email': @record['Email'],
+      'BouncedAt': @record['BouncedAt'],
+      'Description': @record['Description'],
+      'Tag': @record['Tag']
+    }
   rescue JSON::ParserError => e
     print("\nFailed to parse request payload, #{request.body.read}")
     print("\nException: #{e}")
